846409bb850311cda59b26e9fbc036119d5658f9
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-08 20:36:38 +0900

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'articles#index'
   resources :articles, only: [:index, :new, :show]
   resources :users, only: [:index]
+  devise_scope :user do
+    get 'sign_up', to: 'users/registrations#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
 end
