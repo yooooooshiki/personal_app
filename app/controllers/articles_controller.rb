6c41aa75a9215c782692448ebcf77afd21c84266
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-09 16:39:35 +0900

    ページネーション　導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @articles = Article.all
+    @articles = Article.order("created_at DESC").page(params[:page]).per(10)
   end
 
   def new
