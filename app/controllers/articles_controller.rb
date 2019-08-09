5fe6fc1e1980852d55efccb44cd499ca5b44fc5f
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-09 12:17:05 +0900

    dbに保存　完了

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,23 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def new
+    @article = Article.new
   end
+
+  def create
+    @article = Article.new
+    @article.user = current_user 
+    @article.title = params[:article][:title]
+    @article.content = params[:article][:content]
+    @article.save
+    redirect_to root_path
+  end 
+
   def show
   end
+
 end
