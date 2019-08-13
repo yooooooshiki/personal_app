3ea77902488a23e368c42f9ed1d42272df15652f
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-13 17:25:24 +0900

    アソシエーション　変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user, optional: true
+  has_many   :likes, dependent: :destroy
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
