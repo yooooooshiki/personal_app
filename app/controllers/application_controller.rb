c9740ca90fcf445ad46379b179fa90b182c3d821
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-17 14:40:56 +0900

    コントローラーへ記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
+
+    def set_search
+      @q = Article.ransack(params[:q])
+      @article = @q.result(distinct: true)
+    end
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
