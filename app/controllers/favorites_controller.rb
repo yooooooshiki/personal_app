521a7b57a81169225117521c9a46551075c424f2
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-18 23:27:16 +0900

    favoritesコントローラー　変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,18 @@
 class FavoritesController < ApplicationController
-  def
+  def create
     @user_id = session[:id]
     @article_id = Article.find(params[:id]).id
     @favorite = Favorite.new(article_id: @article_id, user_id: @user_id)
     if @favorite.save
       redirect_to articles_path
     end
+    binding.pry
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
+    if @favorite.destroy
+      redirect_to user_path(session[:id])
+    end
   end
-    
 end
