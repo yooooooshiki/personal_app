0cf09bb705794b3c00721d089facf1b4316388d2
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-19 14:30:43 +0900

    session[:id]をcurrent_user.idに変更　記事ストック機能一旦完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,17 @@
 class FavoritesController < ApplicationController
   def create
-    @user_id = session[:id]
+    @user_id = current_user.id
     @article_id = Article.find(params[:id]).id
     @favorite = Favorite.new(article_id: @article_id, user_id: @user_id)
     if @favorite.save
       redirect_to articles_path
     end
-    binding.pry
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     if @favorite.destroy
-      redirect_to user_path(session[:id])
+      redirect_to articles_path
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :articles, through: :favorites
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
