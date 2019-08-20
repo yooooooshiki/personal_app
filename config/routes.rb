18274bd86f533aa0b28e6b5dcad2636cf342fce0
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-20 12:36:30 +0900

    ルーティング　記述

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,15 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :relationships, only: [:create, :destroy]
+
+
   resources :favorites, only: [:destroy]
 
   resources :searches, only: [:index, :show]
