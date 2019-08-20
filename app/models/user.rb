094ab5f4d9ea72af768254b8f371e6d4e04de97e
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-20 12:42:57 +0900

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,23 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :articles, through: :favorites
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    
+      
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    following_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
+  end
+
 end
