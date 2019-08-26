00a67316dc822b8953848d68f13a74b51deb3f95
小松原嘉起 <komatsubarayoshiki@komatsubarayoshimikinoMacBook-Pro.local>
2019-08-26 19:02:56 +0900

    articleモデル　記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,13 @@ class Article < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
   def like_user(user_id)
     likes.find_by(user_id: user_id)
+  end
+
+  def favorite_user(user_id)
+    favorites.find_by(user_id: user_id)
   end
 end
